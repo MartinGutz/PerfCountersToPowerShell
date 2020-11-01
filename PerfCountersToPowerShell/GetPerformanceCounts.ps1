db0977e6daf657ef9c88c0787bc852c28958277e
MartinGutz <MartinGutz@gmail.com>
2020-11-01 06:07:44 -0800

    Add Continuous Parameter
    Add a parameter to add a continuous flag on the script.

--- PerfCountersToPowerShell/GetPerformanceCounts.ps1
+++ PerfCountersToPowerShell/GetPerformanceCounts.ps1
@@ -1,3 +1,7 @@
+param(
+    [string]$IncludeContinuous = $False 
+)
+
 function SanitizeNames($fileName)
 {
     $name = $fileName.ToString()
@@ -10,22 +14,27 @@ function SanitizeNames($fileName)
 }
 
 $counterSetNames = (Get-Counter -ListSet *).CounterSetName
-
 foreach($counterSetName in $counterSetNames)
 {
     $counters = (Get-Counter -ListSet $counterSetName).Counter
     $counterSetName = SanitizeNames $counterSetName
 
-    New-Item -Path .\Counters -Name $counterSetName -ItemType "directory" -Force -Verbose
+    New-Item -Path .\Counters -Name $counterSetName -ItemType "directory" -Force
     foreach($counter in $counters)
     {
         $counterName = $counter.Substring(1)
         $counterName = SanitizeNames $counterName
         $counterFileName = ".\Counters\" + $counterSetName + "\" + $counterName + ".ps1"
         #Write-Host $counterName
-        $contents = 'Get-Counter -Counter ' + '"' + $counter + '"' + ' -Continuous'
-        #$contents = 'powershell.exe -command &{' + '"Get-Counter -Counter ' + '"' + $counter + '"' + ' -Continuous}"'
-        Write-Host $contents
+        #Write-Host "VERBOSE: IncludeContinuous: " $IncludeContinuous 
+        if($IncludeContinuous -eq $True)
+        {
+            $contents = 'Get-Counter -Counter ' + '"' + $counter + '"' + ' -Continuous'
+        }else {
+            $contents = 'Get-Counter -Counter ' + '"' + $counter + '"'
+        }
+        
+        #Write-Host $contents
         # powershell.exe -command "& {get-eventlog -logname security}"
         Set-Content -Path $counterFileName $contents
     }
